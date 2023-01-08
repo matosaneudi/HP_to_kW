c65d8319afaf9950ec1ae0de44e35a3b10a9c097
Aneudi Matos <matosaneudi@gmail.com>
2023-01-08 02:07:08 -0500

    First commit.

---
+++ HP_to_kW_converter.py
@@ -0,0 +1,7 @@
+HP = input('Enter HP amount:')
+kW = float(HP) * .74569987
+print(kW, 'kW')
+
+#TODO: clean up lines 1&2
+#TODO: round up kW to nearest 100
+#TODO: print error message when user inputs anything thats not a int 
